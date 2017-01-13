a666105d8f070c26839609166885e87590239c67
tamas.makkai <tamas.makkai@semilab.hu>
2017-01-13 11:39:47 +0100

    extract ICalc and implement

---
+++ Sample1/Sample1/ICalc.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sample1
+{
+    public interface ICalc
+    {
+        double Add(double a, double b);
+        double Div(double a, double b);
+        double DivB(double a, double b);
+        double DivD(double a, double b);
+    }
+}
